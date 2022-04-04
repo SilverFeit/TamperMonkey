bc8b2b0ca5237556ff528a0dc8eb84a1537e6e34
SilverFeit <danielsanchez.fr@academia.umb.edu.co>
2022-04-04 13:36:16 -0500

    Create r-Condor Template.user.js

---
+++ r-Condor Template.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         r/Condor Template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       B3C3RR0
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://cdn.discordapp.com/attachments/960599137122914304/960602440711929877/condor_back.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
