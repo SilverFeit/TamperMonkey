e1643eac52f78b5957078414aa82110c1593d7f1
SilverFeit <danielsanchez.fr@academia.umb.edu.co>
2022-04-04 17:18:59 -0500

    Update r-Condor Template.user.js

--- r-Condor Template.user.js
+++ r-Condor Template.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/960599137122914304/960614345505640548/colombiar.png";
+            i.src = "https://cdn.discordapp.com/attachments/960630822573068318/960660429506502666/condor2.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
